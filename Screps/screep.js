32b7c51bf6154f1ea344b8694c980c09b66d0d35
Elias Merlo <elimerlof@hotmail.com>
2025-06-19 21:30:08 -0300

    JavaScrep
    Empesamos con ejemplos de Java Screp

---
+++ Screps/screep.js
@@ -0,0 +1,15 @@
+// 
+var VariableLocal;
+let variableGlobal;
+
+alert("alerta de navegador" + CalculoAlert(2,3));
+
+fetch("adadahtml"){
+    
+}
+
+
+function CalculoAlert( puntajeG, puntajeN ){
+    var puntajeT = puntajeG + puntajeN;
+    return puntajeT
+}
